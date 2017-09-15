1c3d865c8878e4e838812a5d0c93678ecddd649b
Simone Stefani <s.stefani95@gmail.com>
2017-09-15 10:33:17 +0200

    Add computation of information gain

--- classification-trees/model.py
+++ classification-trees/model.py
@@ -1,5 +1,6 @@
 import monkdata as m
 import dtree as dt
+from prettytable import PrettyTable
 
 
 ##---------------------------------------------------------
@@ -14,4 +15,22 @@ print ("- Entropy of monk1: ", ent_monk1)
 print ("- Entropy of monk2: ", ent_monk2)
 print ("- Entropy of monk3: ", ent_monk3)
 print ()
+
+
+##---------------------------------------------------------
+# COMPUTE INFORMATION GAIN (ASS 3)
+##---------------------------------------------------------
+print ("Compute information gain of attributes in training datasets:")
+ig_table = PrettyTable(['Dataset', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6'])
+
+for i in range(3):
+  l = ["MONK-{0}".format(i+1)]
+  for k in range(6):
+    gain = dt.averageGain(getattr(m, "monk{0}".format(i+1)), m.attributes[k])
+    l.append(round(gain, 10))
+  ig_table.add_row(l)
+
+print(ig_table)
+
+
 
