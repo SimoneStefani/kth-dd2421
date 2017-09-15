427eff5c27afaa125a218894f2cb263543c820d8
Simone Stefani <s.stefani95@gmail.com>
2017-09-15 10:37:17 +0200

    Print table for entropy

--- classification-trees/model.py
+++ classification-trees/model.py
@@ -6,14 +6,17 @@ from prettytable import PrettyTable
 ##---------------------------------------------------------
 # COMPUTE ENTROPY OF DATASETS (ASS 1)
 ##---------------------------------------------------------
-ent_monk1 = dt.entropy(m.monk1)
-ent_monk2 = dt.entropy(m.monk2)
-ent_monk3 = dt.entropy(m.monk3)
-
 print ("Compute entropy of training datasets:")
-print ("- Entropy of monk1: ", ent_monk1)
-print ("- Entropy of monk2: ", ent_monk2)
-print ("- Entropy of monk3: ", ent_monk3)
+
+ent_table = PrettyTable(['Dataset', 'Entropy'])
+
+for i in range(3):
+  l = ["MONK-{0}".format(i+1)]
+  l.append(round(dt.entropy(getattr(m, "monk{0}".format(i+1))), 10))
+  ent_table.add_row(l)
+
+print(ent_table)
+
 print ()
 
 
