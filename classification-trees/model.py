e56a39d9b697e4b4571291d5c8c661fc26956c51
Simone Stefani <s.stefani95@gmail.com>
2017-09-15 18:43:52 +0200

    Add tree pruning function

--- classification-trees/model.py
+++ classification-trees/model.py
@@ -1,4 +1,4 @@
-from prettytable import pt
+from prettytable import PrettyTable
 import drawtree_qt5 as qt
 import monkdata as m
 import random as rnd
@@ -13,7 +13,7 @@ monks_test = [m.monk1test, m.monk2test, m.monk3test]
 def compute_entropy():
   print ("Compute entropy of training datasets:")
 
-  ent_table = pt(['Dataset', 'Entropy'])
+  ent_table = PrettyTable(['Dataset', 'Entropy'])
 
   for i in range(3):
     l = ["MONK-{0}".format(i+1)]
@@ -30,7 +30,7 @@ def compute_entropy():
 def compute_gain():
   print ("Compute information gain of attributes in training datasets:")
 
-  ig_table = pt(['Dataset', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6'])
+  ig_table = PrettyTable(['Dataset', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6'])
 
   for i in range(3):
     l = ["MONK-{0}".format(i+1)]
@@ -48,7 +48,7 @@ def compute_gain():
 def compute_trees_errors():
   print ("Compute the train and test set errors for the full trees:")
 
-  err_table = pt(['Dataset', 'Error (train)', 'Error (test)'])
+  err_table = PrettyTable(['Dataset', 'Error (train)', 'Error (test)'])
 
   for i in range(3):
     l = ["MONK-{0}".format(i+1)]
@@ -63,8 +63,49 @@ def compute_trees_errors():
   print ()
 
 
+##---------------------------------------------------------
+# PRUNE TREES (ASS 7)
+##---------------------------------------------------------
 def partition(data, fraction):
   ldata = list(data)
-  random.shuffle(ldata)
+  rnd.shuffle(ldata)
   breakPoint = int(len(ldata) * fraction)
   return ldata[:breakPoint], ldata[breakPoint:]
+
+def prune_trees(data, test):
+  fractions = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
+  pruned = []
+
+  for fraction in fractions:
+    train, validate = partition(data, fraction)
+    tree = dt.buildTree(train, m.attributes)
+    forest = dt.allPruned(tree)
+    best_perf = dt.check(tree, validate)
+
+    temp_tree = 0
+    best_tree = tree
+
+    for t in forest:
+      temp_perf = dt.check(t, validate)
+      if best_perf < temp_perf:
+        best_perf = temp_perf
+        best_tree = t
+
+    pruned.append((1 - check(best_tree, test), fraction))
+
+  return pruned
+
+def evaluate_pruning():
+  fractions = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
+  monk1_pruned = []
+  monk3_pruned = []
+
+  for i in range(100):
+    monk1_pruned.append(prune_trees(m.monk1, m.monk1test))
+    monk3_pruned.append(prune_trees(m.monk3, m.monk3test))
+
+  
+
+
+
+  
